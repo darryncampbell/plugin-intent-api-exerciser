6945b1d65d30d6c308618bcf87c9a951d3ac54cc
darryncampbell <jrq768@zebra.com>
2017-04-18 09:44:30 +0100

    Tidied code

--- www/js/index.js
+++ www/js/index.js
@@ -1,21 +1,3 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
 var app = {
     // Application Constructor
     initialize: function() {
@@ -43,22 +25,6 @@ var app = {
         document.getElementById("btnStartActivityPickContact").addEventListener("click", startActivityPickContact);
         document.getElementById("btnStartActivityChooseImage").addEventListener("click", startActivityChooseImage);
 
-
-
-
-        //document.getElementById("testButton").addEventListener("click", testFunctionality);
-        /*window.plugins.intent.setNewIntentHandler(function (intent) {
-            console.log('Received Intent: ' + JSON.stringify(intent.extras));
-            var decodedBarcode = intent.extras["com.symbol.datawedge.data_string"];
-            var parentElement = document.getElementById('barcodeData');
-            if (parentElement && decodedBarcode)
-            {
-                parentElement.innerHTML = "Barcode: " + decodedBarcode;
-                parentElement.setAttribute('style', 'background-color:#0077A0;color:#FFFFFF;');
-            }
-        });
-        */
-
         registerBroadcastReceiver();
         //  Handler for new Intents sent to the application
         window.plugins.intentShim.onIntent(function (intent) {
@@ -67,9 +33,6 @@ var app = {
             var parentElement = document.getElementById('newIntentData');
             parentElement.innerHTML = "Received intent: " + JSON.stringify(intent.extras);
         });
-
-
-
     },
     onPause: function()
     {
@@ -99,7 +62,7 @@ function sendBroadcastTest()
                 }
             },
             function() {},
-            function () {}
+            function () {alert('Failed to send Android broadcast intent')}
         );
 }
 
@@ -137,7 +100,7 @@ function startActivityBrowser()
         url: 'http://www.google.co.uk'
     },
     function() {},
-    function() {}
+    function() {alert('Failed to open URL via Android Intent')}
     );
 }
 
